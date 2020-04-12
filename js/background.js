e50efbecacfff210d3ca770e570e7f462b684c29
Kyle Nelson <nelsonky18@gmail.com>
2020-04-11 20:12:12 -0500

    another final commit

--- js/background.js
+++ js/background.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let title = document.getElementsByTagName('title');
     let body = document.getElementsByTagName('body');
     if(title[0].text == 'AIS Work' || title[0].text == 'Resume') {
-        body[0].style.cssText = " background: url('./images/ais.jpg') no-repeat; background-size: cover;";
+        body[0].style.cssText = "background: url('./images/ais.jpg') no-repeat; background-size: cover;";
     } else if(title[0].text == 'Saving Bees') {
         body[0].style.cssText = "background: url('./images/beecity.jpg') no-repeat; background-size: cover;";
     } else {
